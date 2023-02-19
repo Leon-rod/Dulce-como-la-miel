8f4c38bda2cf4fd5046b872ad7af121ee32cc42d
Leo Rodriguez <leonardoezequielr02@gmail.com>
2023-02-18 21:50:59 -0300

    Dulce como la miel ver. 0.00.0.4

--- code.js
+++ code.js
@@ -1,1 +1,12 @@
-console.log("hello world")
+let menuElements = document.querySelectorAll(".menu-element--click")
+let icon = document.querySelectorAll(".fa-caret-right")
+
+menuElements.forEach(menuElement => {
+    menuElement.addEventListener("click", ()=> {
+        icon.forEach(iconElement => {
+            iconElement.classList.toggle("arrow")
+        })});
+        let height = 0;
+        let menu = menuElement.nextElementSibling;
+        console.log(menu.scrollHeight)
+    });
